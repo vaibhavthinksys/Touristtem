c93013f5e0c9655b55734b06d532d04aa582663b
unknown <thinksysuser@LAP-0082.thinksys.com>
2022-05-24 19:19:24 +0530

    Responsive design

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,12 +11,13 @@ const header = () => {
           alt="logo"
           className="imgLogo"
         />
-        <button className="signupbtn">sign up</button>
+        <button className="signupbtn">Sign Up</button>
+        
       </div>
 
       <div className="abcd">
         <hr className="hrline" />
-        <div className="touristHeading">Schedule Your Tours And Activities</div>
+        <div className="touristHeading"><h3>Schedule Your Tours And Activities</h3></div>
         <div className="mainCityComponent">
           <Citytour />
         </div>
--- src/Components/Maincomponent.js
+++ src/Components/Maincomponent.js
@@ -13,59 +13,66 @@ const Maincomponent = () => {
           <div>Select a Starting Time</div>
 
         </div>
-
-        <div className="totalCompoPart">
-          <div className="calender">
-            <Calendar onChange={onChange} value={value} />
-          </div>
-        </div>
-        <div className="xyz">
-          <div className="availableTime">Available Times:
-          </div>
-          <div className="zyx">
-            <div class="grid-container">
-              <div class="grid-item1">8:00AM</div>
-              <div class="grid-item1"> 8:30AM</div>
-              <div class="grid-item1"> 9:00AM</div>
-              <div class="grid-item1"> 9:30AM</div>
-              <div class="grid-item1"> 10:00AM</div>
-              <div class="grid-item1"> 10:30AM</div>
-              <div class="grid-item1">11:00AM</div>
-              <div class="grid-item1">11:30AM</div>
-              <div class="grid-item1">12:00PM</div>
-              <div class="grid-item1">1:00PM</div>
-              <div class="grid-item1">1:30PM</div>
-              <div class="grid-item1">2:00PM</div>
-              <div class="grid-item1">2:30PM</div>
-              <div class="grid-item1">3:00PM</div>
-              <div class="grid-item1">3:30PM</div>
-              <div class="grid-item1">4:00PM</div>
-              <div class="grid-item1">4:30PM</div>
-              <div class="grid-item1">5:00PM</div>
+        <div className="combined">
+          <div className="totalCompoPart">
+            <div className="calender">
+              <Calendar onChange={onChange} value={value} />
             </div>
           </div>
-          {/* {/ <div>Available Times</div> /} */}
+          <div className="xyz">
+            <div className="availableTime">Available Times:
+            </div>
+            <div className="zyx">
+              <div class="grid-container">
+                <div class="grid-item1">8:00AM</div>
+                <div class="grid-item1"> 8:30AM</div>
+                <div class="grid-item1"> 9:00AM</div>
+                <div class="grid-item1"> 9:30AM</div>
+                <div class="grid-item1"> 10:00AM</div>
+                <div class="grid-item1"> 10:30AM</div>
+                <div class="grid-item1">11:00AM</div>
+                <div class="grid-item1">11:30AM</div>
+                <div class="grid-item1">12:00PM</div>
+                <div class="grid-item1">1:00PM</div>
+                <div class="grid-item1">1:30PM</div>
+                <div class="grid-item1">2:00PM</div>
+                <div class="grid-item1">2:30PM</div>
+                <div class="grid-item1">3:00PM</div>
+                <div class="grid-item1">3:30PM</div>
+                <div class="grid-item1">4:00PM</div>
+                <div class="grid-item1">4:30PM</div>
+                <div class="grid-item1">5:00PM</div>
+              </div>
+            </div>
+            {/* {/ <div>Available Times</div> /} */}
+          </div>
         </div>
 
 
         <div>
           <div className="pickupAddress">Pickup Address(Optional)</div>
-          <div className="inputField"></div>
+          <div className="inputField">
+          <input type="text" placeholder="add"></input>
+          </div>
           <div className="underlineInputField">
             We need the address of your accomodation in order to arrange pickup
           </div>
         </div>
+        <div className="foots">
+          <div className="mobileAndEmail">
+            <div className="mobilePhone">Your mobile phone</div>
+            <div className="inputField1">
+              <input type="text" placeholder="add2"></input>
+            </div>
 
-        <div className="mobileAndEmail">
-          <div className="mobilePhone">Your mobile phone</div>
-          <div className="inputField1">+370 688 8545</div>
-          
-        </div>
+          </div>
 
-        <div className="mobileAndEmail">
-        <div className="mobilePhone1">Your email</div>
-          
-          <div className="inputfield12">darius@gmail.com</div>
+          <div className="mobileAndEmail">
+            <div className="mobilePhone1">Your email</div>
+
+            <div className="inputfield12"><input type="text" placeholder="add3"></input></div>
+
+          </div>
         </div>
 
         <div className="footer">
@@ -73,6 +80,7 @@ const Maincomponent = () => {
             <button className="btn">Save & Book</button>
           </div>
         </div>
+
       </div>
     </div>
   );
